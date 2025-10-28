779361c0ba5a28fd62552bd2f4ce6e323cdf4695
Jothika Pandiyan <jothikapandiyan2002@gamil.com>
2025-10-28 15:35:29 +0530

    Created components and a specific context with related code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+
+import Footer from './components/Footer'
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-     
-  JAAS-Prime-HealthCare
-   
+      <ToastContainer />
+      <Navbar />
+      <Routes>  
+      </Routes>
+      <Footer />
     </div>
   )
 }
