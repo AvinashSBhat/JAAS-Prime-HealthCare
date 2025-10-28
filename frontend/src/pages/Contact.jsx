97a40238996942c195af19350b80b1ab814c65d0
Jothika Pandiyan <jothikapandiyan2002@gamil.com>
2025-10-28 16:23:51 +0530

    Created home and login pages with related code

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+
+
+const Contact = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Contact
---
+++ frontend/src/pages/MyAppointments.jsx
@@ -0,0 +1,18 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { assets } from '../assets/assets'
+
+
+
+const MyAppointments = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyAppointments
