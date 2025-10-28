409399a63b1a36fa9139e7e4508be0167a53aaaf
Jothika Pandiyan <jothikapandiyan2002@gamil.com>
2025-10-28 14:50:14 +0530

    created a frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+     
+  JAAS-Prime-HealthCare
+   
+    </div>
+  )
+}
+
+export default App
