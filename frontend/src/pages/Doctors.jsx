cb5d69f548e9c7a88ed6425e723df830d10fa4b5
Adityakumarsahoo <toadityakumarsahoo@gmail.com>
2025-10-29 17:14:44 +0530

    upload upadated frontend

---
+++ frontend/src/pages/Doctors.jsx
@@ -0,0 +1,15 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useNavigate, useParams } from 'react-router-dom'
+
+
+
+const Doctors = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Doctors
